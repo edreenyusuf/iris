98ccb1e3f2eaddd47c42a9506ff22c1a33855e7d
edreenyusuf <154412074+edreenyusuf@users.noreply.github.com>
2023-12-22 11:55:26 +0800

    Update iris-generate-predictdecisiontreee.py

--- iris-generate-predictdecisiontreee.py
+++ iris-generate-predictdecisiontreee.py
@@ -2,7 +2,6 @@ import streamlit as st
 import pandas as pd
 import pickle
 import numpy
-from sklearn.tree import plot_tree
 
 st.write("# Simple Iris Flower Prediction App")
 st.write("This app predicts the **Iris flower** type!")
